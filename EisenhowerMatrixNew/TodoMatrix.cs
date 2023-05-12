89f9d66d975bed00c971cf6ad8f56ab4e7a17f80
Karol Workowski <charls7c@gmail.com>
2023-05-12 19:26:59 +0200

    up to Class TodoMatrix AddItem(String title, DateTime deadline, bool isImportant) , test

---
+++ EisenhowerMatrixNew/TodoMatrix.cs
@@ -0,0 +1,61 @@
+namespace EisenhowerMatrixNew;
+
+public class TodoMatrix 
+{
+    private Dictionary<string, TodoQuarter> todoQuarters = new Dictionary<string, TodoQuarter>();
+    public TodoMatrix()
+    {
+        todoQuarters.Add("IU", new TodoQuarter());
+        todoQuarters.Add("IN", new TodoQuarter());
+        todoQuarters.Add("NU", new TodoQuarter());
+        todoQuarters.Add("NN", new TodoQuarter());
+    }
+
+    public Dictionary<string, TodoQuarter> GetQuarters()
+    {
+        return todoQuarters;
+    }
+
+    public TodoQuarter GetQuarter(string status)
+    {
+        return todoQuarters[status];
+
+    }
+
+    public void AddItem(string title, DateTime deadline, bool isImportant)
+    {
+        TodoQuarter item = new TodoQuarter();
+        // todoQuarters.Add("IU",item.AddItem(title,deadline));
+       
+       
+    }
+
+    public bool IsUrgent(int id)
+    {
+        TodoQuarter item = new TodoQuarter();
+        TodoItem itemStatus = item.GetItem(id);
+        DateTime itemDeadline = itemStatus.GetDeadline();
+        switch (DateTime.Compare(itemDeadline.Date ,DateTime.Today))
+        {
+            case <=3:
+                return false;
+            case >3:
+                return true;
+            
+        }
+
+
+
+    }
+
+    public string GetStatus(bool isImportant, bool isUrgent ) //TODO 
+    
+    {
+        switch (isImportant 
+        {
+            case "":
+                break;
+        }
+    }
+
+}
