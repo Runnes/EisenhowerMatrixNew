694b5f21840fe24ce5ef606339193965ecc77272
Karol Workowski <charls7c@gmail.com>
2023-05-18 19:52:03 +0200

    ADD working in like 70%

--- EisenhowerMatrixNew/ConnectionSQL.cs
+++ EisenhowerMatrixNew/ConnectionSQL.cs
@@ -3,26 +3,28 @@ using System.Data;
 //using Lib.Extensions.System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Windows;
+using System.Windows.Documents;
+
 namespace EisenhowerMatrixNew;
 
 public class ConnectionSQL
 {
     public void ConnectStringTest()
     {
-        
-    
-    string connetionString = null;
-    SqlConnection cnn = new SqlConnection ();
-    connetionString = "Server= localhost; Database= EisenhowerMatrix; Integrated Security=SSPI;";
-    cnn = new SqlConnection(connetionString);
+
+
+        string connetionString = null;
+        SqlConnection cnn = new SqlConnection();
+        connetionString = "Server= localhost; Database= EisenhowerMatrix; Integrated Security=SSPI;";
+        cnn = new SqlConnection(connetionString);
         try
         {
             cnn.Open();
             Console.WriteLine("udany");
-            
+
             // MessageBox.Show("Connection Open ! ");
             // MessageBox.Show("karol");
-            
+
             cnn.Close();
         }
         catch (Exception ex)
@@ -35,7 +37,7 @@ public class ConnectionSQL
 
     public void ReadTasks()
     {
-        
+
     }
     // public void SQLConnection(string query)
     // {
@@ -54,35 +56,13 @@ public class ConnectionSQL
     //         // MessageBox.Show("Can not open connection ! ");
     //     }
     // }
-    public static string CreateCommand(string queryString)
-    {
-        string connectionString = ConnectionString();
-        using (SqlConnection connection = new SqlConnection(
-                   connectionString))
-        {
-            SqlCommand command = new SqlCommand(queryString, connection);
-            command.Connection.Open();
-            //command.ExecuteReader();
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    string test = reader.ToString();
-                    return test;
-                }
-                
-            }
 
-            return "klarol";
-
-        }
-    }
 
     private static string ConnectionString()
     {
         return "Server= localhost; Database= EisenhowerMatrix; Integrated Security=SSPI;";
     }
-    
+
     public static DataSet GetTasksDataset(string queryString)
     {
         string connectionString = ConnectionString();
@@ -93,14 +73,108 @@ public class ConnectionSQL
             SqlDataAdapter adapter = new SqlDataAdapter();
             adapter.SelectCommand = new SqlCommand(
                 queryString, connection);
-            adapter.Fill(dataset,"TASKS");
+            adapter.Fill(dataset, "TASKS");
             return dataset;
-            
-        }   
+
+        }
     }
 
-    // public static DataRow GetTasksRows(dataset)
-    // {
-    //     dataset = 
-    // }
+    public static DataTable PrintMatrix(DataSet dataset)
+    {
+        DataTable dt = new DataTable();
+        dt.Clear();
+        dt.Columns.Add("Importance");
+        dt.Columns.Add("Urgent");
+        dt.Columns.Add("Not Urgent");
+        bool firstImportant = true;
+        bool firstNonImportant = true;
+
+
+        foreach (DataRow row in dataset.Tables["Tasks"].Rows)
+        {
+            string TaskName = row[1].ToString();
+            if (row[2].ToString() == "Urgent" & row[3].ToString() == "Important" & firstImportant)
+            {
+                DataRow _row = dt.NewRow();
+                _row["Importance"] = "Important";
+                _row["Urgent"] = TaskName;
+                _row["Not Urgent"] = " ";
+                dt.Rows.Add(_row);
+                firstImportant = false;
+            }
+
+            if (row[2].ToString() == "Urgent" & row[3].ToString() == "Important" & !firstImportant)
+            {
+                DataRow _row = dt.NewRow();
+                _row["Importance"] = " ";
+                _row["Urgent"] = TaskName;
+                _row["Not Urgent"] = " ";
+                dt.Rows.Add(_row);
+
+            }
+
+            if (row[2].ToString() == "Not Urgent" & row[3].ToString() == "Important" & firstImportant)
+            {
+                DataRow _row = dt.NewRow();
+                _row["Importance"] = "Important";
+                _row["Urgent"] = " ";
+                _row["Not Urgent"] = TaskName;
+                dt.Rows.Add(_row);
+                firstImportant = false;
+            }
+
+            if (row[2].ToString() == "Not Urgent" & row[3].ToString() == "Important" & firstNonImportant)
+            {
+                DataRow _row = dt.NewRow();
+                _row["Importance"] = "Not Important";
+                _row["Urgent"] = " ";
+                _row["Not Urgent"] = TaskName;
+                dt.Rows.Add(_row);
+                firstNonImportant = false;
+            }
+
+            if (row[2].ToString() == "Urgent" & row[3].ToString() == "Important" & !firstNonImportant)
+            {
+                DataRow _row = dt.NewRow();
+                _row["Importance"] = " ";
+                _row["Urgent"] = TaskName;
+                _row["Not Urgent"] = " ";
+                dt.Rows.Add(_row);
+            }
+
+        }
+
+        return dt;
+
+    }
+
+    public static void AddTask(int TaskID, string TaskName, string TaskImportance, DateTime TaskDeadline)
+    {
+        string connectionString = ConnectionString();
+        string TaskUrgency = "Urgent"; //TO DO Placeholder
+        string queryString =
+            "INSERT INTO TASKS(TaskID, TaskName,TaskImportance,TaskUrgency,TaskDeadline) VALUES(@TaskID,@TaskName,@TaskImportance,@TaskUrgency,@TaskDeadline)";
+
+        //DataSet dataset = new DataSet("Tasks");
+        using (SqlConnection connection =
+               new SqlConnection(connectionString))
+        {
+            
+            SqlCommand _query = new SqlCommand(queryString, connection);
+            _query.Parameters.AddWithValue("@TaskID", TaskID);
+            _query.Parameters.AddWithValue("@TaskName", TaskName);
+            _query.Parameters.AddWithValue("@TaskImportance", TaskImportance);
+            _query.Parameters.AddWithValue("@TaskUrgency", TaskUrgency);
+            _query.Parameters.AddWithValue("@TaskDeadline", TaskDeadline);
+            
+            
+            _query.Connection.Open();
+            _query.ExecuteNonQuery();
+            _query.Connection.Close();
+
+
+        }
+        Console.WriteLine("Done adding");
+    }
 }
+
--- EisenhowerMatrixNew/Program.cs
+++ EisenhowerMatrixNew/Program.cs
@@ -1,17 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Data;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.JavaScript;
+using System.Xml.Schema;
 using EisenhowerMatrixNew;
+using Spectre.Console;
+using SpectreTable = Spectre.Console.Table;
+using Spectre.Console.Extensions.Table;
 
-
+ConnectionSQL.AddTask(5,"TestADD","Not Important",DateTime.Now);
 DataSet data = ConnectionSQL.GetTasksDataset( "SELECT * FROM TASKS");
+var table = ConnectionSQL.PrintMatrix(data);
 DataTable dt = data.Tables["TASKS"];
 
-foreach (DataRow dr in dt.Rows)
-{
- Console.WriteLine(dr["TaskName"].ToString());
-}
+var table2 = table.FromDataTable().Border(TableBorder.Rounded);
+AnsiConsole.Render(table2);
+
+// AnsiConsole.Write(table);
+// AnsiConsole.Write(table);
+//Console.WriteLine(table.Rows[0][0]);
+
+// foreach (DataRow dr in dt.Rows)
+// {
+//  
+//  // Console.WriteLine(dr["TaskId"].ToString(),dr["TaskName"],dr["TaskUrgency"].ToString(),dr["TaskImportance"].ToString(),dr["TaskDeadline"].ToString(),dr["TaskStatusDone"].ToString(),dr["TaskStatusArchived"].ToString());
+//
+// }
 
 
 
